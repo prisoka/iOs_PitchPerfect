882a5e15a6fbc2e0a94758cc3c2ac6ca981d01f7
Priscilla Okawa <prisoka@gmail.com>
2018-08-23 16:24:02 -0700

    Initial Commit

---
+++ iOs_PitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOs_PitchPerfect
+//
+//  Created by Priscilla Okawa on 23/8/18.
+//  Copyright © 2018 Priscilla Okawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
