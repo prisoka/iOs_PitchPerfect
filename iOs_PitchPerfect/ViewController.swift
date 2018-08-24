ba910200df0bb1535309e49a97b9bbb4548709e7
Priscilla Okawa <prisoka@gmail.com>
2018-08-24 11:19:17 -0700

    user IBOutlet to reach back to label UI element

--- iOs_PitchPerfect/ViewController.swift
+++ iOs_PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,6 +25,7 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(_ sender: Any) {
         print ("Record button was pressed")
+        recordingLabel.text = "Recording in Progress"
     }
 }
 
