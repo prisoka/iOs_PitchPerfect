2979a4598a0df2354db87462e79aab3a5ff0653f
Priscilla Okawa <prisoka@gmail.com>
2018-08-24 14:13:31 -0700

    fixed ViewController file

--- iOs_PitchPerfect/ViewController.swift
+++ iOs_PitchPerfect/ViewController.swift
@@ -11,14 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
-    
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBOutlet weak var stopRecordingButton: UIButton!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
